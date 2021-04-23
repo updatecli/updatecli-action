a01e9f49baa137e0ff7a5983bf67c77b1a2e800d
Olivier Vernin <olivier@vernin.me>
2021-04-23 09:20:36 +0200

    Use entrypoint script
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

---
+++ github-actions-entrypoint.bash
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+set -euo pipefail
+
+ARGS=""
+
+if test -n "${UPDATECLI_COMMAND}"; then
+  ARGS="$UPDATECLI_COMMAND"
+else
+  ARGS="help"
+fi
+
+if test -n "${UPDATECLI_FLAGS}"; then
+  ARGS="$ARGS $UPDATECLI_FLAGS"
+fi
+
+# Convert ARGS to an array so we can quote the variable
+# passed to updatecli as command parameter
+
+IFS=" " read -r -a ARGS <<< "$ARGS"
+
+updatecli "${ARGS[@]}"
