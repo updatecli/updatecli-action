0458da25009d10a5374cfb6d911c9946befc6dd0
Joseph Petersen <josephp90@gmail.com>
2022-07-22 19:40:50 +0200

    code cleanup, add unit tests, hook up eslint and prettier (#100)
    * cleanup logic and hook up eslint and prettier
    
    * core.addPath is called in updatecliVersion
    
    * convert to module and use unicorn
    
    * format
    
    * tool-cache has sensible default for arch
    
    * move core.addPath to updatecliDownload
    
    * syntax
    
    * syntax and prefer linux first in conditions
    
    * dist
    
    * fully testable
    
    * fix lint
    
    * cleanup test code
    
    * fix import
    
    * run tests
    
    * make method testable when throwing exception
    
    * dist
    
    * check exitcode for run
    
    * fix exit code test
    
    * add updatecli target

---
+++ src/main.js
@@ -0,0 +1,102 @@
+import core from '@actions/core'
+import tool from '@actions/tool-cache'
+import exec from '@actions/exec'
+import path from 'node:path'
+
+export async function updatecliExtract(downloadPath, downloadUrl) {
+  if (downloadUrl.endsWith('.tar.gz')) {
+    return tool.extractTar(downloadPath)
+  } else if (downloadUrl.endsWith('.zip')) {
+    return tool.extractZip(downloadPath)
+  } else {
+    throw new Error(`Unsupported archive type: ${downloadUrl}`)
+  }
+}
+
+// download Updatecli retrieve updatecli binary from Github Release
+export async function updatecliDownload() {
+  const version = core.getInput('version')
+
+  const updatecliPackages = [
+    {
+      arch: 'x64',
+      platform: 'linux',
+      url: `https://github.com/updatecli/updatecli/releases/download/${version}/updatecli_Linux_x86_64.tar.gz`,
+    },
+    {
+      arch: 'arm64',
+      platform: 'linux',
+      url: `https://github.com/updatecli/updatecli/releases/download/${version}/updatecli_Linux_arm64.tar.gz`,
+    },
+    {
+      arch: 'x64',
+      platform: 'win32',
+      url: `https://github.com/updatecli/updatecli/releases/download/${version}/updatecli_Windows_x86_64.zip`,
+    },
+    {
+      arch: 'arm64',
+      platform: 'win32',
+      url: `https://github.com/updatecli/updatecli/releases/download/${version}/updatecli_Windows_arm64.zip`,
+    },
+    {
+      arch: 'x64',
+      platform: 'darwin',
+      url: `https://github.com/updatecli/updatecli/releases/download/${version}/updatecli_Darwin_x86_64.tar.gz`,
+    },
+    {
+      arch: 'arm64',
+      platform: 'darwin',
+      url: `https://github.com/updatecli/updatecli/releases/download/${version}/updatecli_Darwin_arm64.tar.gz`,
+    },
+  ]
+
+  const updatecliPackage = updatecliPackages.find(
+    x => x.platform == process.platform && x.arch == process.arch
+  )
+  if (!updatecliPackage) {
+    throw new Error(
+      `Unsupported platform ${process.platform} and arch ${process.arch}`
+    )
+  }
+
+  core.info(`Downloading ${updatecliPackage.url}`)
+  const downloadPath = await tool.downloadTool(updatecliPackage.url)
+
+  core.debug(`Extracting file ${downloadPath} ...`)
+  const updatecliExtractedFolder = await updatecliExtract(
+    downloadPath,
+    updatecliPackage.url
+  )
+  core.debug(`Extracted file to ${updatecliExtractedFolder} ...`)
+
+  core.debug('Adding to the cache ...')
+  const cachedPath = await tool.cacheDir(
+    updatecliExtractedFolder,
+    'updatecli',
+    version,
+    process.arch
+  )
+
+  if (process.platform == 'linux' || process.platform == 'darwin') {
+    await exec.exec('chmod', ['+x', path.join(cachedPath, 'updatecli')])
+  }
+
+  core.addPath(cachedPath)
+
+  core.info(`Downloaded to ${cachedPath}`)
+}
+
+export async function updatecliVersion() {
+  core.info('Show Updatecli version')
+  await exec.exec('updatecli version')
+}
+
+export async function run() {
+  try {
+    await updatecliDownload()
+    await updatecliVersion()
+    process.exitCode = core.ExitCode.Success
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
---
+++ tests/main.test.js
@@ -0,0 +1,142 @@
+import path from 'node:path'
+import url from 'node:url'
+import {promises as fs} from 'node:fs'
+import {
+  run,
+  updatecliDownload,
+  updatecliVersion,
+  updatecliExtract,
+} from 'src/main'
+import {ExitCode} from '@actions/core'
+
+const directory = path.dirname(url.fileURLToPath(import.meta.url))
+
+const cachePath = path.join(directory, 'CACHE')
+const temporaryPath = path.join(directory, 'TEMP')
+// Set temp and tool directories before importing (used to set global state)
+process.env['RUNNER_TEMP'] = temporaryPath
+process.env['RUNNER_TOOL_CACHE'] = cachePath
+
+const version = 'v0.28.0'
+const versionWithoutV = version.slice(1)
+
+process.env['INPUT_VERSION'] = version
+const originalPlatform = process.platform
+const originalArch = process.arch
+
+const restorePlatformArch = () => {
+  Object.defineProperty(process, 'platform', {
+    value: originalPlatform,
+  })
+  Object.defineProperty(process, 'arch', {
+    value: originalArch,
+  })
+}
+const fakePlatformArch = (fakePlatform, fakeArch) => {
+  Object.defineProperty(process, 'platform', {
+    value: fakePlatform,
+  })
+  Object.defineProperty(process, 'arch', {
+    value: fakeArch,
+  })
+}
+
+describe('main', () => {
+  it('run', async () => {
+    await run()
+    const file = path.join(
+      cachePath,
+      'updatecli',
+      versionWithoutV,
+      process.arch,
+      'updatecli'
+    )
+    const fileStat = await fs.stat(file)
+    expect(fileStat.isFile()).toBe(true)
+    expect(process.exitCode).toBe(ExitCode.Success)
+  })
+
+  it('unknown extract', async () => {
+    await expect(
+      updatecliExtract('/tmp/foo', 'foo.bar')
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported archive type: foo.bar"`
+    )
+  })
+
+  it('unknown platform', async () => {
+    fakePlatformArch('foo', 'bar')
+    await expect(
+      updatecliDownload()
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported platform foo and arch bar"`
+    )
+    restorePlatformArch()
+  })
+
+  it('updatecli not found', async () => {
+    const path = process.env['PATH']
+    process.env['PATH'] = ''
+    await expect(updatecliVersion()).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Unable to locate executable file: updatecli. Please verify either the file path exists or the file can be found within a directory specified by the PATH environment variable. Also check the file mode to verify the file is executable."`
+    )
+    process.env['PATH'] = path
+  })
+
+  it('run unknown platform', async () => {
+    fakePlatformArch('foo', 'bar')
+    await run()
+    expect(process.exitCode).toBe(ExitCode.Failure)
+    restorePlatformArch()
+  })
+
+  it('linux should download', async () => {
+    fakePlatformArch('linux', 'x64')
+    await updatecliDownload()
+    const file = path.join(
+      cachePath,
+      'updatecli',
+      versionWithoutV,
+      process.arch,
+      'updatecli'
+    )
+    const fileStat = await fs.stat(file)
+    expect(fileStat.isFile()).toBe(true)
+    restorePlatformArch()
+  })
+
+  it('windows should download', async () => {
+    fakePlatformArch('win32', 'x64')
+    await updatecliDownload()
+    const file = path.join(
+      cachePath,
+      'updatecli',
+      versionWithoutV,
+      process.arch,
+      'updatecli.exe'
+    )
+    const fileStat = await fs.stat(file)
+    expect(fileStat.isFile()).toBe(true)
+    restorePlatformArch()
+  })
+
+  it('darwin should download', async () => {
+    fakePlatformArch('darwin', 'x64')
+    await updatecliDownload()
+    const file = path.join(
+      cachePath,
+      'updatecli',
+      versionWithoutV,
+      process.arch,
+      'updatecli'
+    )
+    const fileStat = await fs.stat(file)
+    expect(fileStat.isFile()).toBe(true)
+    restorePlatformArch()
+  })
+})
+
+afterAll(async () => {
+  await fs.rm(temporaryPath, {recursive: true})
+  await fs.rm(cachePath, {recursive: true})
+})
