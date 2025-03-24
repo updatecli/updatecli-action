d67c5c02dbe8fde894f047648dd37b862fc3c509
updatecli < updatecli-bot@updatecli.io>
2025-03-24 07:51:55 +0000

    chore: deps: bump updatecli version in src/main.js to v0.96.0
    Made with ❤️️ by updatecli

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import exec from '@actions/exec'
 import path from 'node:path'
 import fs from 'node:fs'
 
-const DEFAULT_VERSION = `v0.95.1`
+const DEFAULT_VERSION = `v0.96.0`
 
 // get the Updatecli version from the action inputs
 export async function getUpdatecliVersion() {
